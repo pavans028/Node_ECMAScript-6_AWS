f2449a2209045cb212621867f40dbef3a6e8b3e5
Pavankumar Bhujanga Shetty <pavanshetty028@gmail.com>
2017-04-28 15:11:31 -0500

    Connect to MongoDB (create and find)
    - Use native mongodb npm module
    - Use object destruction ES6 feature to extract only {MongoClient} from mongodb module
    - Create collections & documents, find documents
    - Play around with ObjectID and cursor from mongodb find method ( new ObjectID('_id') )

---
+++ Node-Mongo/playground/mongodb-connect.js
@@ -0,0 +1,34 @@
+// const MongoClient = require('mongodb').MongoClient;
+const {MongoClient, ObjectID} = require('mongodb');
+
+MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, db) => {
+  if (err) {
+    return console.log('Unable to connect to MongoDB server');
+  }
+  console.log('Connected to MongoDB server');
+
+  db.collection('Todos').insertOne({
+    text: 'Finish Node and Start React',
+    completed: false
+  }, (err, result) => {
+    if (err) {
+      return console.log('Unable to insert todo', err);
+    }
+  
+    console.log(JSON.stringify(result.ops, undefined, 2));
+  });
+
+  Insert new doc into Users (name, age, location)
+  db.collection('Users').insertOne({
+    name: 'Pavan',
+    age: 25,
+    location: 'Chicago'
+  }, (err, result) => {
+    if (err) {
+      return console.log('Unable to insert user', err);
+    }
+    console.log(result.ops[0]._id.getTimestamp()); // in built function
+  });
+
+  db.close();
+});
